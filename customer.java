603aa6f5f3923e9247430ceef0a12a712f36831e
batheand <batheand@gmail.com>
2022-12-24 00:14:56 +0300

    array implementation

--- customer.java
+++ customer.java
@@ -1,22 +1,12 @@
 public class customer {
     private Integer priorityLevel = 0, id = 0, orderTime = 0, preparationTime = 0;
 
-    public void setPriorityLevel(Integer priorityLevel) {
-        this.priorityLevel = priorityLevel;
+    public customer (int id, int priorityLevel, int orderTime, int preparationTime) {
+        this.id=id;
+        this.priorityLevel=priorityLevel;
+        this.orderTime=orderTime;
+        this.preparationTime=preparationTime;
     }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setOrderTime(Integer orderTime) {
-        this.orderTime = orderTime;
-    }
-
-    public void setPreparationTime(Integer preparationTime) {
-        this.preparationTime = preparationTime;
-    }
-
     public Integer getPriorityLevel() {
         return priorityLevel;
     }
--- heapPQ.java
+++ heapPQ.java
@@ -1,61 +1,47 @@
+import java.io.*;
+
 public class heapPQ {
     int size;
-    node root;
-    public void initialize(String fileName) {
+    customer [] pq;
+    public void heapPQ(String fileName) {
+        // This will reference one line at a time
+        String line = null;
+        try {
+            // FileReader reads text files in the default encoding
+            FileReader fileReader = new FileReader(fileName);
 
-    }
-    public void queue(node x) {
-        getLast().setNext(x);
-        size++;
+            // Wrap FileReader in BufferedReader
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            line = bufferedReader.readLine().trim();
+            size = Integer.parseInt(line);
+            // create a new array with the length we found
+            pq = new customer[size];
+            // Read each non-blank line of the file and store in the array
+            for (int i = 0; i<size; i++) {
+                line = bufferedReader.readLine().trim();
+                String [] butcheredInfo = line.split(" ");
+                if (butcheredInfo.length==4) {
+                    customer temp = new customer(Integer.parseInt(butcheredInfo[0]), 2022-Integer.parseInt(butcheredInfo[1]), Integer.parseInt(butcheredInfo[2]), Integer.parseInt(butcheredInfo[3]));
+                    pq [i] = temp;
+                }
+            }
+            // Close the file
+            bufferedReader.close();
+        } catch (IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");
+        }
     }
     public void swimUp(int index) {
-        while (index > 1 && index(index).getCustomer().getId()>index(index/2).getCustomer().getId()) {
-            swap(index(index), index(index/2));
-            index = index/2;
+        if(size>index) {
+            while (index > 1 && pq[index].getPriorityLevel()>pq[index/2].getPriorityLevel()) {
+                customer temp = pq[index];
+                pq[index] = pq[index/2];
+                pq[index/2] = temp;
+                index = index/2;
+            }
         }
     }
     public void sinkDown() {}
-
-    public void swap (node a, node b) {
-        // If x and y are the same node, there is nothing to do
-    if (a == b) {
-        return;
-      }
-  
-      // Find the previous nodes of x and y
-      node prevA = null, prevB = null;
-      node curr = root;
-      while (curr != null) {
-        if (curr.getNext() == a) {
-          prevA = curr;
-        }
-        if (curr.getNext() == b) {
-          prevB = curr;
-        }
-        curr = curr.getNext();
-      }
-  
-      // If one of the nodes was not found, there is nothing to do
-      if (prevA == null || prevB == null) {
-        return;
-      }
-  
-      // Swap the data of the nodes
-      customer temp = a.getCustomer();
-      a.setCustomer(b.getCustomer());
-      b.setCustomer(temp);
-    }
-    public node getLast() { return index(size); }
-    public node index(int k) {
-        node currNode = root;
-        if (k>size) {
-            return null;
-        } else {
-            for (int i = 0; i<k-1; i++) {
-                currNode = currNode.getNext();
-            }
-            return currNode;
-        }
-    }
+    
     
 }
