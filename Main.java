de371e9120a0c72f3b3eb26f38d122b2f7034ac7
batheand <batheand@gmail.com>
2022-12-25 16:47:58 +0300

    test merged

--- Main.java
+++ Main.java
@@ -1,5 +1,41 @@
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
-        heapPQ pq = new heapPQ("sampleinput1.txt");
+        customer [] customerList = txtToArray("sampleinput1.txt");
+        heapPQ pq = new heapPQ(customerList.length);
+        pq.simulateTest(customerList, 10);
+    }
+
+    public static customer [] txtToArray(String fileName) {
+        // This will reference one line at a time
+        String line = null;
+        int size = 1;
+        try {
+            // FileReader reads text files in the default encoding
+            FileReader fileReader = new FileReader(fileName);
+
+            // Wrap FileReader in BufferedReader
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            line = bufferedReader.readLine().trim();
+            size = Integer.parseInt(line)+1;
+            // create a new array with the length we found
+            //pq = new customer[size];
+            customer [] temp = new customer [size];
+            // Read each non-blank line of the file and store in the array
+            for (int i = 1; i<size; i++) {
+                line = bufferedReader.readLine().trim();
+                String [] butcheredInfo = line.split(" ");
+                if (butcheredInfo.length==4) {
+                    temp [i] = new customer(Integer.parseInt(butcheredInfo[0]), 2022-Integer.parseInt(butcheredInfo[1]), Integer.parseInt(butcheredInfo[2]), Integer.parseInt(butcheredInfo[3]));
+                    //temp[i].print();
+                }
+            }
+            // Close the file
+            bufferedReader.close();
+            return temp;
+        } catch (IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");
+            return null;
+        }
     }
 }
--- heapPQ.java
+++ heapPQ.java
@@ -59,6 +59,104 @@ public class heapPQ {
             index = j;
 }
     }
+
+    public void simulateTest (customer [] customerList, int maxAvgWait) {
+        //enlist the current time customers, use the current number of couriers
+        //operate on the current customers based on the priority level
+        //delete the customers taken care of
+        //increment time
+        //store the serving times, take avg compare to maxAvgWait
+        //if bigger increment the courier count and try again
+        //if less or equal print the needed courier count and the simulation
+        int time = 1;
+        double avgTime=0;
+        int[] times = new int [customerList.length];
+        int i=1;
+        int courierCount = 0;
+
+        do { //until you find the correct courierCount
+            //initialize the couriers
+            courierCount++;
+            while (pq[1]!=null) {
+                couriers [] allCouriers = new couriers [courierCount];
+                for (int j = 0; j<courierCount; j++) {
+                   couriers temp = new couriers();
+                    allCouriers [j] = temp; 
+                }
+                while (i<customerList.length) {
+                    //enlist the current time customers
+                    while (customerList[i].getOrderTime()==time) {
+                        enlist(customerList[i]);i++;
+                    } 
+                    i++;
+                    times = blockAvailableCouriers(allCouriers, times, time,true);
+                }
+            }
+                int totalTime = 0;
+                for (int j = 1; j<customerList.length; j++) {
+                    System.out.println(times [j]);
+                    totalTime += times[j];
+                }
+                avgTime = totalTime/customerList.length;
+            } while (avgTime>maxAvgWait);
+            simulate(customerList, courierCount);
+    }
+    public void simulate (customer [] customerList, int courierCount) {
+        //enlist the current time customers, use the current number of couriers
+        //operate on the current customers based on the priority level
+        //delete the customers taken care of
+        //increment time
+        //store the serving times, take avg compare to maxAvgWait
+        //if bigger increment the courier count and try again
+        //if less or equal print the needed courier count and the simulation
+        int time = 1;
+        double avgTime=0;
+        int[] times = new int [customerList.length];
+        int i=1;
+
+            while (pq[1]!=null) {
+                            //initialize the couriers
+                couriers [] allCouriers = new couriers [courierCount];
+                for (int j = 0; j<courierCount; j++) {
+                    couriers temp = new couriers();
+                    allCouriers [j] = temp; 
+                }
+                while (i<customerList.length) {
+                    //enlist the current time customers
+                    while (customerList[i].getOrderTime()==time) {
+                        enlist(customerList[i]); i++;
+                    }
+                    i++;
+                    times = blockAvailableCouriers(allCouriers, times, time, false);
+                    allCouriers = updateCouriers(allCouriers, time);
+                }
+            }
+
+            
+    }
+    public int [] blockAvailableCouriers (couriers [] allCouriers, int [] times, int time, boolean test) {
+        for (int i = 0; i<allCouriers.length; i++){
+            if(allCouriers[i].isAvailable) {
+                customer temp = delMaxPriority();
+                times[temp.getId()] = time-temp.getOrderTime();
+                System.out.println(times[temp.getId()]);
+                allCouriers[i].blockCourier(time, temp.getPreparationTime());
+                //delete the root and block the courier
+                //assign the root's time to times 
+                if(!test) {
+                    System.out.println("Courier " + i + " takes customer " + temp.getId() 
+                    + " at minute " + time + " (wait: " + times[temp.getId()] + " mins");
+                }
+            }
+        }
+        return times;
+    }
+    public couriers[] updateCouriers(couriers [] c, int time) {
+        for (int i = 0; i<c.length;i++) {
+            c[i].renewAvailability(time);
+        }
+        return c;
+    }
     public void print() {
         System.out.println("\n\n NEWEST VERSION OF THE LIST \n\n");
         for (int i = 1; i<currSize; i++) {
